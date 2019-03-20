35f8c18966dd160f1c366f03b1d276ba131adfa5
Ayhan Akilli <info@ayhan.biz>
2019-03-20 17:36:41 +0100

    Test: Add section media, rename classes and filter output in downcast

--- plugin.js
+++ plugin.js
@@ -24,25 +24,31 @@
             editor.widgets.add('section', {
                 button: editor.lang.section.title,
                 dialog: 'section',
-                template: '<section class=""><h2>Title</h2><div class="content"></div></section>',
+                template: '<section class=""><h2>Title</h2><div class="section-media"></div><div class="section-content"></div></section>',
                 editables: {
                     title: {
                         selector: 'h2',
-                        allowedContent: {}
+                        allowedContent: 'a[!href]'
+                    },
+                    media: {
+                        selector: '.section-media',
+                        allowedContent: 'figure',
+                        requiredContent: 'figure'
                     },
                     content: {
-                        selector: '.content'
+                        selector: '.section-content'
                     }
                 },
-                allowedContent: 'section(*); h2; div(content)',
-                requiredContent: 'section; h2; div(content)',
+                allowedContent: 'section(*); h2; div(!section-media); div(!section-content)',
+                requiredContent: 'section; h2; div(section-media); div(section-content)',
                 upcast: function (el) {
                     if (el.name !== 'section' || !intersect(cfg, el.classes)) {
                         return false;
                     }
 
                     var title = el.getFirst('h2');
-                    var content = new CKEDITOR.htmlParser.element('div', {'class': 'content'});
+                    var media = new CKEDITOR.htmlParser.element('div', {'class': 'section-media'});
+                    var content = new CKEDITOR.htmlParser.element('div', {'class': 'section-content'});
 
                     if (!!title && title.children.length > 0 && title.children[0].type === CKEDITOR.NODE_ELEMENT) {
                         title.setHtml(title.children[0].getHtml());
@@ -56,11 +62,12 @@
                         el.add(title, 0);
                     }
 
-                    el.add(content, 1);
+                    el.add(media, 1);
+                    el.add(content, 2);
 
-                    if (el.children.length > 2) {
-                        content.children = el.children.slice(2);
-                        el.children = el.children.slice(0, 2);
+                    if (el.children.length > 3) {
+                        content.children = el.children.slice(3);
+                        el.children = el.children.slice(0, 3);
                     }
 
                     return true;
@@ -70,11 +77,33 @@
                         return new CKEDITOR.htmlParser.text('');
                     }
 
-                    el.children = el.children.slice(0, 1);
-                    el.children[0].attributes = [];
-                    el.setHtml(el.getHtml() + this.editables.content.getData());
+                    // Content
+                    el.children[2].setHtml(this.editables.content.getData());
+                    el.children[2].children.forEach(function (item) {
+                        if (item.getHtml().trim()) {
+                            el.add(item);
+                        }
+                    });
+                    el.children[2].remove();
 
-                    return el;
+                    // Media
+                    el.children[1].setHtml(this.editables.media.getData());
+                    var media = el.children[1].getFirst('figure');
+
+                    if (media) {
+                        el.children[1].replaceWith(media);
+                    } else {
+                        el.children[1].remove();
+                    }
+
+                    // Title
+                    if (!el.children[0].getHtml().trim()) {
+                        el.children[0].remove();
+                    } else {
+                        el.children[0].attributes = [];
+                    }
+
+                    return el.children.length > 0 ? el : new CKEDITOR.htmlParser.text('');
                 },
                 data: function () {
                     if (this.data.type) {
