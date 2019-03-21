3bfa42fe7f7e0d2cd3f0ca134ef21bf6020d3616
Ayhan Akilli <info@ayhan.biz>
2019-03-21 21:37:19 +0100

    Temp. commit: Add plugin stylesheet

--- plugin.js
+++ plugin.js
@@ -113,6 +113,7 @@
             });
 
             CKEDITOR.dialog.add('section', this.path + 'dialogs/section.js');
+            editor.addContentsCss(this.path + 'styles/section.css');
         }
     });
 })(CKEDITOR);
