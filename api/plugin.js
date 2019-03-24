6826801b44bab1169ec7379b5d39543798e81877
Ayhan Akilli <info@ayhan.biz>
2019-03-24 10:35:47 +0100

    Fix + refactor

--- api/plugin.js
+++ api/plugin.js
@@ -16,8 +16,41 @@
 
     /**
      * Public API
+     *
+     * @class
+     * @singleton
      */
     CKEDITOR.api = {
+        /**
+         * Sends a XMLHttpRequest with given method and returns the response as text
+         *
+         * @param {string} method
+         * @param {string} url
+         * @param {string|null} body
+         *
+         * @return {string|null}
+         */
+         request: function (method, url, body) {
+            if (!method || !url) {
+                return null;
+            }
+
+            var xhr = new XMLHttpRequest();
+
+            try {
+                xhr.open(method, url, false);
+                xhr.send(body);
+
+                if (xhr.readyState === xhr.DONE && xhr.status >= 200 && xhr.status < 300) {
+                    return xhr.responseText;
+                }
+            } catch (e) {
+                console.log(e);
+            }
+
+            return null;
+        },
+
         /**
          * Sends a XMLHttpRequest with the GET method
          *
@@ -26,7 +59,7 @@
          * @return {string|null}
          */
         get: function (url) {
-            return ajax('GET', url, null);
+            return CKEDITOR.api.request('GET', url, null);
         },
 
         /**
@@ -37,7 +70,7 @@
          * @return {string|null}
          */
         head: function (url) {
-            return ajax('HEAD', url, null);
+            return CKEDITOR.api.request('HEAD', url, null);
         },
 
         /**
@@ -49,21 +82,38 @@
          * @return {string|null}
          */
         post: function (url, body) {
-            return ajax('POST', url, body);
+            return CKEDITOR.api.request('POST', url, body);
         },
 
         /**
-         * Returns origin from given URL
+         * Opens a browser window with given name and executes given callback function when a message from browser
+         * window is received and closes the browser window
          *
          * @param {string} url
-         *
-         * @return {string}
+         * @param {string} name
+         * @param {function} call
          */
-        origin: function (url) {
-            var a = document.createElement('a');
-            a.href = url;
+        browser: function (url, name, call) {
+            if (!url || !name || typeof call !== 'function') {
+                return;
+            }
 
-            return a.origin;
+            var win = CKEDITOR.api.popup(url, name, null);
+            var origin;
+
+            try {
+                origin = win.origin || win.location.origin;
+            } catch (e) {
+                console.log(e);
+                origin = CKEDITOR.api.origin(url);
+            }
+
+            window.addEventListener('message', function (ev) {
+                if (ev.origin === origin && ev.source === win) {
+                    call(ev.data);
+                    win.close();
+                }
+            }, false);
         },
 
         /**
@@ -80,64 +130,17 @@
         },
 
         /**
-         * Opens a browser window with given name and executes given callback function when a message from browser
-         * window is received and closes the browser window
+         * Returns origin from given URL
          *
          * @param {string} url
-         * @param {string} name
-         * @param {function} call
+         *
+         * @return {string}
          */
-        browser: function (url, name, call) {
-            if (!url || !name || typeof call !== 'function') {
-                return;
-            }
+        origin: function (url) {
+            var a = document.createElement('a');
+            a.href = url;
 
-            var win = this.popup(url, name, null);
-            var origin;
-
-            try {
-                origin = win.origin || win.location.origin;
-            } catch (e) {
-                console.log(e);
-                origin = this.getOrigin(url);
-            }
-
-            window.addEventListener('message', function (ev) {
-                if (ev.origin === origin && ev.source === win) {
-                    call(ev.data);
-                    win.close();
-                }
-            }, false);
+            return a.origin;
         }
     };
-
-    /**
-     * Sends a XMLHttpRequest with given method and returns the response as text
-     *
-     * @param {string} method
-     * @param {string} url
-     * @param {string|null} body
-     *
-     * @return {string|null}
-     */
-    function ajax(method, url, body) {
-        if (!method || !url) {
-            return null;
-        }
-
-        var xhr = new XMLHttpRequest();
-
-        try {
-            xhr.open(method, url, false);
-            xhr.send(body);
-
-            if (xhr.readyState === xhr.DONE && xhr.status >= 200 && xhr.status < 300) {
-                return xhr.responseText;
-            }
-        } catch (e) {
-            console.log(e);
-        }
-
-        return null;
-    }
 })(window, document, console, XMLHttpRequest, CKEDITOR);
