c23ab8da1075490e45e6977ab7f21671d1911184
Ayhan Akilli <info@ayhan.biz>
2019-03-24 17:16:38 +0100

    Remove deprecated CKEDITOR.mediabrowser public api

--- mediabrowser/plugin.js
+++ mediabrowser/plugin.js
@@ -15,49 +15,6 @@
     CKEDITOR.on('dialogDefinition', dialogDefinition);
 
     /**
-     * Public API
-     *
-     * @class
-     * @singleton
-     * @deprecated Use api plugin {@see CKEDITOR.api} directly
-     */
-    CKEDITOR.mediabrowser = {
-        /**
-         * Opens a mediabrowser window and executes given callback function when a message from the mediabrowser window
-         * is received and closes the browser window
-         *
-         * @param {string} url
-         * @param {function} call
-         */
-        open: function (url, call) {
-            CKEDITOR.api.browser(url, 'mediabrowser', call);
-        },
-
-        /**
-         * Opens a new mediabrowser window with given options
-         *
-         * @param {string} url
-         * @param {string|null} opts
-         *
-         * @return {Window}
-         */
-        popup: function (url, opts) {
-            return CKEDITOR.api.popup(url, 'mediabrowser', opts);
-        },
-
-        /**
-         * Returns origin from given URL
-         *
-         * @param {string} url
-         *
-         * @return {string}
-         */
-        getOrigin: function (url) {
-            return CKEDITOR.api.origin(url);
-        }
-    };
-
-    /**
      * Listener for dialogDefinition event
      *
      * @param {CKEDITOR.eventInfo} ev
